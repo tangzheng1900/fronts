4dc3d3e7bab6844284bc060d3c6c75e774005aee
tangzheng1900 <tangzheng1900@163.com>
2016-03-05 16:38:40 +0800

    add my eleme project

---
+++ eleme/js/basic.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: john
+ * Date: 16-3-5
+ * Time: 上午10:42
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+function showmessage(){
+    alert("运行到这里都是正确的！！");
+
+}
