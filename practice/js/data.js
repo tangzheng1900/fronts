899f25791e7223ecea33e258e291a156ffb43502
tangzheng1900 <tangzheng1900@163.com>
2016-03-08 21:59:06 +0800

    test code

---
+++ practice/js/data.js
@@ -0,0 +1,17 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: john
+ * Date: 16-3-8
+ * Time: 下午8:32
+ * To change this template use File | Settings | File Templates.
+ */
+
+person{
+    name:tang;
+    age:24;
+}
+
+
+var pernum=new Array();
+pernum.push({name:don;
+             age:46});
